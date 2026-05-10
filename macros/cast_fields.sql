7cab233dca45a7f86eb1d2a9ba6002183f92fc0e
shivam4fractal <shivam.agrawal3@fractal.ai>
2026-05-11 00:01:37 +0530

    Auto-generate dbt project (7 artifacts) - 2026-05-10 18:31 UTC

--- macros/cast_fields.sql
+++ macros/cast_fields.sql
@@ -1,3 +1,5 @@
-{% macro cast_fields(field, type) %}
-  cast({{ field }} as {{ type }})
+{% macro cast_fields(field_map) %}
+  {% for field, dtype in field_map.items() %}
+    cast({{ field }} as {{ dtype }}) as {{ field }}{% if not loop.last %}, {% endif %}
+  {% endfor %}
 {% endmacro %}
