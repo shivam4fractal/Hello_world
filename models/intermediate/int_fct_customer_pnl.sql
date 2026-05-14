33208774bc258318e8193ef67b204d9a3571e969
shivam4fractal <shivam.agrawal3@fractal.ai>
2026-05-15 00:01:21 +0530

    Auto-generate dbt project (7 artifacts) - 2026-05-14 18:31 UTC

--- models/intermediate/int_fct_customer_pnl.sql
+++ models/intermediate/int_fct_customer_pnl.sql
@@ -1,18 +1,24 @@
 with acdoca as (
-  select * from {{ ref("stg_amer_nafpa__acdoca") }}
-)
+  select * from {{ ref('stg_amer_nafpa__acdoca') }}
+),
+kpi_map as (
+  select * from {{ source('amer_h_manual_mst', 't_nala_cstmr_pnl_kpi_mapping') }}
+),
+ska1 as (
+  select * from {{ source('amer_h_saps4_am', 't_ska1') }}
+),
 select
-  FARM_FINGERPRINT(CONCAT(concat(To_date(extract(year from cast(amer_h_saps4_am.t_acdoca.budat as date));extract(month from cast(amer_h_saps4_am.t_acdoca.budat as date));'01')))) as cal_sk,
-  FARM_FINGERPRINT(CONCAT(FARM_FINGERPRINT( CONCAT( amer_h_saps4_am.t_acdoca.ktopl; '|'; amer_h_saps4_am.t_acdoca.rbukrs; '|'; amer_h_saps4_am.t_acdoca.racct;  '|'; amer_h_saps4_am.t_ska1.bilkt;    '|'; amer_h_saps4_am.t_ska1.ktoks;    '|'; amer_h_saps4_am.t_ska1.gvtyp;    '|'; 'SAPS4_AM_NA')) as account_cust_pnl_sk,
-  FARM_FINGERPRINT(CONCAT(farm_fingerprint(CONCAT(amer_h_saps4_am.t_acdoca.prctr; '|';  amer_h_saps4_am.t_acdoca.rcntr;'|'; amer_h_saps4_am.t_acdoca.rbukrs ; '|'; amer_dc_md.t_dim_country.country_cd_nk ; '|' ; 'SAPS4_AM_NA')))) as fi_org_sk,
-  FARM_FINGERPRINT(CONCAT(farm_fingerprint(amer_h_saps4_am.t_acdoca.kunnr))) as customer_sk,
-  FARM_FINGERPRINT(CONCAT(farm_fingerprint(amer_h_saps4_am.t_acdoca.matnr)unionfarm_fingerprint(amer_h_saps4_am.t_acdoca.subbrand))) as product_sk,
-  FARM_FINGERPRINT(CONCAT(farm_fingerprint(amer_h_saps4_am.t_acdoca.lifnr))) as vendor_sk,
-  FARM_FINGERPRINT(CONCAT(farm_fingerprint(amer_h_saps4_am.t_acdoca.matnr_copa))) as product_sold_sk,
-  FARM_FINGERPRINT(CONCAT(farm_fingerprint(concat(amer_h_manual_mst.t_nala_cstmr_pnl_kpi_mapping.kpi_name;amer_h_manual_mst.t_nala_cstmr_pnl_kpi_mapping.cluster_code_nk; amer_h_manual_mst.t_nala_cstmr_pnl_kpi_mapping.gl_account;'SAPS4_AM_NA'))) as pnl_kpi_sk,
-  FARM_FINGERPRINT(CONCAT(farm_fingerprint(concat(amer_h_saps4_am.t_acdoca.rhcur;amer_h_saps4_am.t_acdoca.rkcur;amer_h_saps4_am.t_acdoca.rwcur;amer_h_saps4_am.t_acdoca.runit;'SAPS4_AM_NA')))) as units_sk,
-  FARM_FINGERPRINT(CONCAT(farm_fingerprint(amer_h_saps4_am.t_acdoca.werks))) as plant_sk,
-  FARM_FINGERPRINT(CONCAT(farm_fingerprint(concat(amer_h_saps4_am.t_acdoca.matnr; amer_h_saps4_am.t_acdoca.vkorg;amer_h_saps4_am.t_acdoca.vtweg)))) as customer_salesarea_sk,
+  {{ farm_fingerprint([cal_sk]) }} as cal_sk,
+  {{ farm_fingerprint([account_cust_pnl_sk]) }} as account_cust_pnl_sk,
+  {{ farm_fingerprint([fi_org_sk]) }} as fi_org_sk,
+  {{ farm_fingerprint([customer_sk]) }} as customer_sk,
+  {{ farm_fingerprint([product_sk]) }} as product_sk,
+  {{ farm_fingerprint([vendor_sk]) }} as vendor_sk,
+  {{ farm_fingerprint([product_sold_sk]) }} as product_sold_sk,
+  {{ farm_fingerprint([pnl_kpi_sk]) }} as pnl_kpi_sk,
+  {{ farm_fingerprint([units_sk]) }} as units_sk,
+  {{ farm_fingerprint([plant_sk]) }} as plant_sk,
+  {{ farm_fingerprint([customer_salesarea_sk]) }} as customer_salesarea_sk,
   amer_h_saps4_am.t_acdoca.msl as kpi_value_base_uom,
   "acdoca.msl * (c.denominator_conversion_base_units_measure_qt / c.numerator_conversion_base_units_measure_qt) as kpi_value_cse; from amer_h_saps4_am.t_acdoca.t_acdoca as a left join t_dim_material_uom  as c on a.matnr = c.material_cd_nk and c.alternate_uom_cd = ""CSE"" where c.alternate_uom_cd is not null" as kpi_value_cse,
   "acdoca.msl * (l.denominator_conversion_base_units_measure_qt / l.numerator_conversion_base_units_measure_qt) as kpi_value_cse; from amer_h_saps4_am.t_acdoca.t_acdoca as a left join t_dim_material_uom  as l on a.matnr = l.material_cd_nk and c.alternate_uom_cd = ""LB"" where l.alternate_uom_cd is not null" as kpi_value_lbs,
@@ -21,3 +27,5 @@ select
   amer_h_saps4_am.t_acdoca.hsl as lcl_curr_kpi_value,
   amer_h_saps4_am.t_acdoca.wsl as txn_curr_kpi_value
 from acdoca
+left join kpi_map on ...
+left join ska1 on ...
