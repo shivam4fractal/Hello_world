3fda155669044645d7ac7ecb7ff1732f98f66cc0
shivam4fractal <shivam.agrawal3@fractal.ai>
2026-05-27 00:01:37 +0530

    Auto-generate dbt project (7 artifacts) - 2026-05-26 18:31 UTC

--- models/marts/mart_customer_pnl.sql
+++ models/marts/mart_customer_pnl.sql
@@ -1,26 +1,11 @@
-with fct as (
+with fact as (
   select * from {{ ref("int_fct_customer_pnl") }}
 ),
 product as (
   select * from {{ ref("int_dim_product_cust_pnl") }}
-),
-kpi as (
-  select * from {{ ref("int_dim_pnl_kpi") }}
-),
-fi_org as (
-  select * from {{ ref("int_dim_fi_org") }}
-),
-account as (
-  select * from {{ ref("int_dim_account_cust_pnl") }}
 )
 select
-  fct.*,
-  product.*,
-  kpi.*,
-  fi_org.*,
-  account.*
-from fct
-left join product on fct.product_id = product.product_id
-left join kpi on fct.kpi_id = kpi.kpi_id
-left join fi_org on fct.company_code = fi_org.company_code
-left join account on fct.account_id = account.account_id
+  fact.*,
+  product.*
+from fact
+left join product on fact.product_id = product.product_id
